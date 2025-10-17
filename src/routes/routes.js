bbc0a167ca15674f9fb4ae3bc372db8bf73d8d53
Adriyan <adrianadrian.aa613@gmail.com>
2025-10-17 20:46:26 +0700

    feat(getAllBooksHandler): Menambahkan endpoint GET untuk menampilkan semua buku

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
 const addBooksHandler = require('../handlers/addBooksHandler');
+const getAllBooksHandler = require('../handlers/getAllBooksHandler');
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
   },
 ];
 
